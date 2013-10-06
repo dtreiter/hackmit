04368e99a60a5a22591263852f7587bed49e157c
ymmn <abdnimery92@gmail.com>
2013-10-05 22:59:58 -0500

    code to create insert statements

---
+++ insert_code_maker.py
@@ -0,0 +1,9 @@
+import json
+f = open("song-to-mov.json", "r")
+d = json.loads(f.read())
+code = ""
+for sid in d:
+	code += ("$client->putItem(array('TableName' => 'songs', 'Item' => array('song_id' => array(Type::STRING => '%s') , 'movies' => array(Type::STRING => '%s') ) ));\n" % (sid, str(d[sid])))
+f = open("insert.php", "w")
+f.write(code)
+f.close()
