1570ab10a1574fdb5764a4d6810cdc76444d5e0e
Ubuntu <ubuntu@ip-172-31-18-89.us-west-2.compute.internal>
2013-10-06 10:16:25 +0000

    adjusted insert code to remove dupes

--- insert_code_maker.py
+++ insert_code_maker.py
@@ -1,10 +1,12 @@
 import json
+from sets import Set
 f = open("song-to-mov.json", "r")
 d = json.loads(f.read())
 code = ""
 for sid in d:
 	for i in range(len(d[sid])):
 		d[sid][i] = d[sid][i].encode("utf-8")
+	d[sid] = list(Set(d[sid]))
 	code += ("$client->putItem(array('TableName' => 'songs', 'Item' => array('song_id' => array(Type::STRING => \"%s\") , 'movies' => array(Type::STRING => \"%s\") ) ));\n" % (sid, str(d[sid])))
 f = open("insert.php", "w")
 f.write(code)
